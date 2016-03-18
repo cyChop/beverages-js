79a42a975adcf7432d5d851bbcbd3459fb356cee
Cyrille Chopelet <cyrille.chopelet@mines-nancy.org>
2016-03-18 18:57:42 +0100

    [#13] Review the template
    The basics are all right now. Will refine it later

--- src/main/js/lib/rivets-cfg.js
+++ src/main/js/lib/rivets-cfg.js
@@ -5,27 +5,45 @@ define([
     'rivets-backbone-adapter'
 ], function (rivets, _) {
     'use strict';
-    
+
     /* Make sure Array.isArray is available (should be). */
     if (!Array.isArray) {
-        Array.isArray = function(arg) {
+        Array.isArray = function (arg) {
             return Object.prototype.toString.call(arg) === '[object Array]';
         };
     }
 
+    /* === Arrays === */
+
     rivets.formatters.join = function (array, separator) {
-        return isArray(array) ? array.join(separator) : array;
+        return Array.isArray(array) ? array.join(separator) : array;
     };
 
     rivets.formatters.contains = function (array, needle) {
-        return isArray(array) ? _.indexOf(array, needle) > -1 : false;
+        return Array.isArray(array) ? _.indexOf(array, needle) > -1 : false;
     };
+
+    rivets.formatters.isEmpty = function (array) {
+        return !Array.isArray(array) || array.length === 0;
+    }
+
+    /* === String === */
+
+    rivets.formatters.startWithCap = function (string) {
+        return typeof string === 'string' && string.length > 0
+            ? string.charAt(0).toUpperCase() + string.substr(1)
+            : string;
+    };
+
+    /* === Utils === */
 
     rivets.formatters.not = function (value) {
         return !value;
     };
 
-    rivets.formatters.unit = function(value, unit) {
+    /* === Misc === */
+
+    rivets.formatters.unit = function (value, unit) {
         return value || value === 0 ? value + unit : value;
     };
 
