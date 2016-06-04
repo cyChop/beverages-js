c398f8ed16adc0f65d7676194bd153ac93a5c74b
Cyrille Chopelet <cyrille.chopelet@mines-nancy.org>
2016-06-04 10:33:46 +0200

    Diff missing from last commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,22 @@
 const gulp = require('gulp'),
     gutil = require('gulp-util'),
     rimraf = require('gulp-rimraf'),
+    path = require('path'),
+    eslint = require('gulp-eslint'),
     webpack = require('webpack'),
-    WebpackDevServer = require('webpack-dev-server'),
-    path = require('path');
+    WebpackDevServer = require('webpack-dev-server');
 
 /* === CONFIG === */
 const cfg = require('./webpack.config.js');
 
 /* === TASKS === */
+gulp.task('lint', function () {
+    return gulp.src(['src/**/*.js', '!node_modules/**'])
+        .pipe(eslint())
+        .pipe(eslint.format('stylish'))
+        .pipe(eslint.failAfterError());
+});
+
 gulp.task('clean', function () {
     return gulp.src(cfg.output.path, {read: false}).pipe(rimraf());
 });
@@ -53,3 +61,5 @@ gulp.task('webserver-dev', function () {
 gulp.task('webserver-dev-offline', ['_webpack:offline', 'webserver-dev']);
 
 gulp.task('default', ['build']);
+
+gulp.task('ci', ['lint', 'build']);
